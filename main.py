9ee78170d7c4781ad52e3c31117d6583cea593d2
Dmitrium12 <belicdima8@gmail.com>
2022-11-17 18:25:06 +0700

    задача 3

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import random
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5 import uic
+from UI import Ui_MainWindow
 from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
+        self.color_mass = [Qt.yellow, Qt.green, Qt.red, Qt.blue, Qt.black, Qt.cyan]
         self.pushButton.clicked.connect(self.run)
 
     def paintEvent(self, event):
         if self.do_paint:
             qp = QPainter(self)
-            qp.setPen(QPen(Qt.yellow,  8, Qt.SolidLine))
+            qp.setPen(QPen(random.choice(self.color_mass),  8, Qt.SolidLine))
             chislo = random.randint(100, 400)
             qp.drawEllipse(40, 40, chislo, chislo)
             qp.end()
