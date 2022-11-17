42a30d6fea726019dfc948ece743da1419615632
Dmitrium12 <belicdima8@gmail.com>
2022-11-17 18:44:52 +0700

    задача 4

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-import random
+import sqlite3
 import sys
-from PyQt5.QtCore import Qt
-from UI import Ui_MainWindow
-from PyQt5.QtGui import QPainter, QPen
-from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-class MyWidget(QMainWindow, Ui_MainWindow):
+class DBSample(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.do_paint = False
-        self.color_mass = [Qt.yellow, Qt.green, Qt.red, Qt.blue, Qt.black, Qt.cyan]
-        self.pushButton.clicked.connect(self.run)
+        uic.loadUi('main.ui', self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.select_data()
 
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter(self)
-            qp.setPen(QPen(random.choice(self.color_mass),  8, Qt.SolidLine))
-            chislo = random.randint(100, 400)
-            qp.drawEllipse(40, 40, chislo, chislo)
-            qp.end()
+    def select_data(self):
+        res = self.connection.cursor().execute("SELECT * from coffee").fetchall()
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
 
-    def run(self):
-        self.do_paint = True
-        self.repaint()
+    def closeEvent(self, event):
+        # При закрытии формы закроем и наше соединение
+        # с базой данных
+        self.connection.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = DBSample()
     ex.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
